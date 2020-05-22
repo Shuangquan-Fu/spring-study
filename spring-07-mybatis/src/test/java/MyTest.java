9e2770d8ee01841de037d8af926ad1131fa20446
shufu <fsq19981210@gmail.com>
2020-05-22 16:22:10 -0400

    learning

--- spring-07-mybatis/src/test/java/MyTest.java
+++ spring-07-mybatis/src/test/java/MyTest.java
@@ -1,10 +1,12 @@
 import com.kuang.mapper.UserMapper;
+import com.kuang.mapper.UserMapperImpl;
 import com.kuang.pojo.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import sun.security.util.Resources_zh_TW;
 
 import java.io.IOException;
@@ -14,11 +16,9 @@ import java.util.List;
 public class MyTest {
     @Test
     public void test() throws IOException {
-        String resource = "Mybatis.xml";
-        InputStream in = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(in);
-        SqlSession session = sessionFactory.openSession(true);
-        UserMapper mapper = session.getMapper(UserMapper.class);
+
+        ClassPathXmlApplicationContext cont = new ClassPathXmlApplicationContext("spring-dao.xml");
+        UserMapperImpl mapper = (UserMapperImpl) cont.getBean("userMapper");
         List<User> users = mapper.selectUser();
         for (User user : users) {
             System.out.println(user);
